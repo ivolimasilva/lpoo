101a259aad306bef3003768adbf914e575864865
Ivo Lima da Silva <ivo.lima.da.silva@gmail.com>
2015-03-07 17:47:59 +0000

    1ª Iteração - Alínea 2
    Movimento do herói e verificação de chegada à Saída.

--- lpoo_1/src/lpoo_1/maze_game.java
+++ lpoo_1/src/lpoo_1/maze_game.java
@@ -1,11 +1,16 @@
 package lpoo_1;
 
+import java.util.*;
+
 public class maze_game
 {
+	
+	// Variáveis do labirinto
+	
 	static char maze_map[][] =
 	{
 		{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
-		{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+		{ 'X', 'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
 		{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
 		{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
 		{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
@@ -16,9 +21,32 @@ public class maze_game
 		{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' }
 	};
 	
+	// Variáveis do Herói
+	
+	static int hero_x = 1, hero_y = 1;
+	static char hero_state = 'H'; // pode ser 'H' (normal), 'A' (armado) ou ' ' (morto);
+	
+	// Variáveis da Saída
+	
+	static int exit_x = 5, exit_y = 9;
+	
+	
 	public static void main(String[] args)
 	{
-		printMaze();
+		while (!gameOver())
+		{
+			printMaze();
+			moveHero();
+		}
+		System.out.println("Fim de jogo!");
+	}
+	
+	public static boolean gameOver()
+	{
+		if (hero_x == exit_x && hero_y == exit_y)
+			return true;
+		else
+			return false;
 	}
 	
 	public static void printMaze()
@@ -30,5 +58,62 @@ public class maze_game
 			
 			System.out.println();
 		}	
+	}
+	
+	public static void moveHero()
+	{
+		boolean keyIsViable = false;
+		char key;
+		while (!keyIsViable)
+		{
+			key = readKey();
+			
+			keyIsViable = true;
+			
+			if (key == 'w' && movementViable(hero_x - 1, hero_y))
+			{
+				maze_map[hero_x][hero_y] = ' ';
+				hero_x--;
+				maze_map[hero_x][hero_y] = hero_state;
+			}
+			else if (key == 'd' && movementViable(hero_x, hero_y + 1))
+			{
+				maze_map[hero_x][hero_y] = ' ';
+				hero_y++;
+				maze_map[hero_x][hero_y] = hero_state;
+			}
+			else if (key == 's' && movementViable(hero_x + 1, hero_y))
+			{
+				maze_map[hero_x][hero_y] = ' ';
+				hero_x++;
+				maze_map[hero_x][hero_y] = hero_state;
+			}
+			else if (key == 'a' && movementViable(hero_x, hero_y - 1))
+			{
+				maze_map[hero_x][hero_y] = ' ';
+				hero_y--;
+				maze_map[hero_x][hero_y] = hero_state;
+			}
+			else if (key != 'w' && key != 'd' && key != 's' && key != 'a')
+			{
+				System.out.println("Direcção inválida, insira outra.");
+				keyIsViable = false;
+			}
+		}
+	}
+	
+	public static boolean movementViable(int pos_x, int pos_y)
+	{
+		if (maze_map[pos_x][pos_y] != 'X')
+			return true;
+		else
+			return false;
+	}
+	
+	public static char readKey()
+	{
+		System.out.print("Insira uma direcção (W, A, S, D): ");
+		Scanner c = new Scanner(System.in);
+		return c.next().charAt(0);
 	}
 }
